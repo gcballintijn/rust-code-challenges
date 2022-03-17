347576956143379eca5992057231909cfa7b0f91
Gerco Ballintijn <gerco@ballintijn.com>
2022-03-17 23:59:42 +0100

    My solution to  1.1, a bit overkill.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,50 @@
+use std::cmp::Ordering;
+
+#[derive(Clone, Copy)]
+struct MyF32(f32);
+
+impl Ord for MyF32 {
+    fn cmp(&self, other: &Self) -> Ordering {
+        if self.0 < other.0 {
+            Ordering::Less
+        } else if self.0 > other.0 {
+            Ordering::Greater
+        } else {
+            Ordering::Equal
+        }
+    }
+}
+
+impl Eq for MyF32 {}
+
+impl PartialOrd for MyF32 {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.0.partial_cmp(&other.0)
+    }
+}
+
+impl PartialEq for MyF32 {
+    fn eq(&self, other: &MyF32) -> bool {
+        self.0 == other.0
+    }
+}
+
 fn median(a: Vec<f32>) -> Option<f32> {
-    todo!();
+    if a.is_empty() {
+        return None;
+    }
+
+    let mut a: Vec<MyF32> = a.iter().map(|f| MyF32(*f)).collect();
+    a.sort();
+
+    let middle = a.len() / 2;
+    if a.len() % 2 == 0 {
+        let first = a[middle - 1];
+        let second = a[middle];
+        Some((first.0 + second.0) / 2.0)
+    } else {
+        Some(a[middle].0)
+    }
 }
 
 fn main() {

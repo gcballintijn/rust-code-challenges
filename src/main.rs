0d320a6c266115d9e143f173b65bb3d498e69474
Gerco Ballintijn <gerco@ballintijn.com>
2022-03-21 00:50:28 +0100

    My solutions to 1.3.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
-fn info(a: &T) {
-    todo!();
+// use std::fmt::Display;
+// fn info<T>(text: &T)
+//     where T: Display
+// {
+//     println!("{}", text.to_string());
+// }
+
+// fn info<T>(text: &T)
+//     where T: ToString
+// {
+//     println!("{}", text.to_string());
+// }
+
+// fn info<T>(text: &T)
+//     where T: AsRef<str>
+// {
+//     println!("{}", text.as_ref());
+// }
+
+fn info<T>(text: &T)
+    where T: Into<Vec<u8>> + Clone
+{
+    let x: Vec<u8> = text.to_owned().into();
+    let s = String::from_utf8(x).unwrap();
+    println!("{}", s);
 }
 
 fn main() {
@@ -9,17 +32,15 @@ fn main() {
     info(&b);
 
     // Advanced 1
-    // use std::ffi::CString;
-    
-    // let c = CString::new("?").unwrap();
-    // info(&input);
+    use std::ffi::CString;    
+    let c = CString::new("?").unwrap();
+    info(&c);
 
     // Advanced 2
     // use std::path::Path;
     // let d = Path::new("/tmp/linkedin-learning");
-    // info(d);
+    // info(&d);
 }
-
 
 #[test]
 fn str() {
@@ -39,16 +60,16 @@ fn string() {
 //     info(&input);
 // }
 
-// #[test]
-// fn cstring() {
-//     use std::ffi::{CString};
-//     let input = CString::new("Rust").unwrap();
-//     info(&input);
-// }
+#[test]
+fn cstring() {
+    use std::ffi::{CString};
+    let input = CString::new("Rust").unwrap();
+    info(&input);
+}
 
 // #[test]
 // fn path() {
 //     use std::path::Path;
 //     let input = Path::new("/tmp/rust");
-//     info(input);
+//     info(&input);
 // }

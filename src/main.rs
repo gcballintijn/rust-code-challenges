6b367bc7dbf8f387681007eac4d632b3818f465b
Gerco Ballintijn <gerco@ballintijn.com>
2022-03-22 13:56:32 +0100

    My solution to 1.6.

--- src/main.rs
+++ src/main.rs
@@ -30,8 +30,43 @@ impl Hand {
     }
 
     fn value(&self) -> usize {
-        // TODO: implement this method
-        0 
+        use Card::*;
+        
+        // Compute minimal hand value and count aces
+        let mut min_hand_value = 0;
+        let mut ace_count = 0;
+        for card in &self.cards {
+            let min_card_value = match card {
+                Ace=> { ace_count += 1; 1 },
+                Two => 2,
+                Three => 3,
+                Four => 4,
+                Five => 5,
+                Six => 6,
+                Seven => 7,
+                Eight => 8,
+                Nine => 9,
+                Jack => 10,
+                Queen => 10,
+                King => 10,                            
+            };
+            min_hand_value += min_card_value;
+        }
+
+        let mut result = min_hand_value;
+        for _ in 0..ace_count {
+            if result >= 21 {
+                break;
+            }
+
+            result = if (result + 10) <= 21 {
+                result + 10
+            } else {
+                result
+            };
+        }
+
+        result
     }
 
     fn is_loosing_hand(&self) -> bool {
@@ -43,6 +78,21 @@ fn main() {
     let mut hand = Hand::new();
     hand.add(Card::King);
     hand.add(Card::Ace);
+
+    println!("The value of King and Ace is {} and this {} a loosing hand.",
+        hand.value(),
+        if hand.is_loosing_hand() { "IS" } else { "is not" }
+    );
+
+    let mut hand = Hand::new();
+    hand.add(Card::King);
+    hand.add(Card::Queen);
+    hand.add(Card::Two);
+
+    println!("The value of King, Queen, and Two is {} and this {} a loosing hand.",
+        hand.value(),
+        if hand.is_loosing_hand() { "IS" } else { "is not" }
+    );
 }
 
 
@@ -81,4 +131,45 @@ fn oops() {
     
     assert!(hand.is_loosing_hand());
     assert_eq!(hand.value(), 22);
+}
+
+#[test]
+fn low_hand() {
+    let mut hand = Hand::new();
+    hand.add(Card::Three);
+    hand.add(Card::Three);
+    hand.add(Card::Seven);
+    
+    assert_eq!(hand.value(), 13);
+}
+
+#[test]
+fn four_aces() {
+    let mut hand = Hand::new();
+    hand.add(Card::Ace);
+    hand.add(Card::Ace);
+    hand.add(Card::Ace);
+    hand.add(Card::Ace);
+    
+    assert_eq!(hand.value(), 14);
+}
+
+#[test]
+fn is_loosing_hand() {
+    let mut hand = Hand::new();
+    hand.add(Card::King);
+    hand.add(Card::Queen);
+    hand.add(Card::Two);
+    
+    assert!(hand.is_loosing_hand());
+}
+
+#[test]
+fn is_not_loosing_hand() {
+    let mut hand = Hand::new();
+    hand.add(Card::King);
+    hand.add(Card::Seven);
+    hand.add(Card::Two);
+    
+    assert!(!hand.is_loosing_hand());
 }

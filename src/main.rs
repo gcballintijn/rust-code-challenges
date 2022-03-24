3292e0e541174599d6316a577e34fc5f1eff158f
Gerco Ballintijn <gerco@ballintijn.com>
2022-03-24 17:02:51 +0100

    My solution to 1.13.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::fmt::Display;
 use std::str::FromStr;
 
 #[derive(Debug, PartialEq)]
-struct Rgb; // TODO: design data structure
+struct Rgb(u8, u8, u8);
 
 trait RgbChannels {
     fn r(&self) -> u8;
@@ -13,11 +13,58 @@ trait RgbChannels {
 }
 
 impl RgbChannels for Rgb {
-    // TODO: implement trait
+    fn r(&self) -> u8 {
+        self.0
+    }
+
+    fn g(&self) -> u8 {
+        self.1
+    }
+
+    fn b(&self) -> u8 {
+        self.2
+    }
+}
+
+#[derive(Debug)]
+enum RgbError {
+    TooShort,
+    TooLong,
+    NoHash,
+    InvalidLiteral,
 }
 
 impl FromStr for Rgb {
-    // TODO: implement trait
+    type Err = RgbError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.len() < 7 {
+            return Err(RgbError::TooShort);
+        } else if s.len() > 7 {
+            return Err(RgbError::TooLong);
+        }
+
+        let mut iter = s.chars();
+        if iter.next().unwrap() != '#' {
+            return Err(RgbError::NoHash)
+        }
+
+        for ch in iter {
+            match ch.to_ascii_lowercase() {
+                '0'..='9' => {}
+                'a'..='f' => {}
+                _ => {
+                    return Err(RgbError::InvalidLiteral)
+                } 
+            }            
+        }
+
+        let r: u8 = u8::from_str_radix(&s[1..3], 16).unwrap();
+        let g: u8 = u8::from_str_radix(&s[3..5], 16).unwrap();
+        let b: u8 = u8::from_str_radix(&s[5..7], 16).unwrap();
+
+        Ok(Self(r, g, b))
+    }
 }
 
 impl Display for Rgb {
